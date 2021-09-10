bb13c7b62f80adeb19983321744ca594bb7d0775
Raphael Cohn <raphael.cohn@stormmq.com>
2021-09-10 18:15:35 +0100

    Typo

--- workspace/usb-storm/src/device/binary_object_store/platform_device_capability/PlatformDeviceCapability.rs
+++ workspace/usb-storm/src/device/binary_object_store/platform_device_capability/PlatformDeviceCapability.rs
@@ -2,7 +2,7 @@
 // Copyright © 2021 The developers of security-keys-rust. See the COPYRIGHT file in the top-level directory of this distribution and at https://raw.githubusercontent.com/lemonrock/security-keys-rust/master/COPYRIGHT.
 
 
-/// Mandatory for hubs.
+/// Platform device capability.
 #[derive(Debug, Clone, Ord, PartialOrd, Eq, PartialEq, Hash)]
 #[derive(Deserialize, Serialize)]
 #[serde(deny_unknown_fields)]
